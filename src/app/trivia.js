7cf118f2cf9ddfbbc683a8da12e61fde8157df6d
Richard <lapislacrima@gmail.com>
2017-01-11 21:42:57 -0800

    rearrange divs
    hint now on top of questions
    extraneous classless divs deleted

--- src/app/trivia.js
+++ src/app/trivia.js
@@ -2,42 +2,28 @@ import React, {Component} from 'react';
 import {Questions} from './questions.js';
 import Hint from './hint.js';
 
-const styles = {
-  container: {
-    border: '0px solid red',
-    borderRadius: '6px',
-    maxHeight: '540px',
-    padding: '0px 10px',
-    overflowX: 'hidden',
-    overflowY: 'auto'
-  }
-};
 
 export class Trivia extends Component {
   render() {
     return (
-      <div>
-        <div style={styles.container}>
+      <div className="questions-container">
+        <div className="question-describe">
           Which city is being described?
         </div>
-        <div style={styles.container}>
-          <div>
-            <Questions
-              hintCount={this.props.stateData.hintCount}
-              questionList={this.props.stateData.questionList}
-              newQuestion={this.props.newQuestion}
-              gameState={this.props.stateData.gameState}
-              />
-          </div>
-        </div>
-        <div className="btn-group">
+        <div className="btn-group btn-div">
           <Hint
             addHint={this.props.addHint}
             hintCount={this.props.stateData.hintCount}
             gameState={this.props.stateData.gameState}
             />
         </div>
-      </div>
+          <Questions
+            hintCount={this.props.stateData.hintCount}
+            questionList={this.props.stateData.questionList}
+            newQuestion={this.props.newQuestion}
+            gameState={this.props.stateData.gameState}
+            />
+        </div>
     );
   }
 }
